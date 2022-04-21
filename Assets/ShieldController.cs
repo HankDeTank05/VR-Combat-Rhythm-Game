3184c4668150f540e5540039629b73c661899628
HankDeTank05 <henryhankholman@gmail.com>
2022-04-21 13:37:44 -0500

    upgraded project to 2021.1.17 and aligned the sword and shield to the controllers

---
+++ Assets/ShieldController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour
+{
+    public GameObject leftController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.position = leftController.transform.position;
+    }
+}
---
+++ Assets/SwordController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    public GameObject rightController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.position = rightController.transform.position;
+    }
+}
